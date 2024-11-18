ad6a4a4d626b6eb83b32d5cae0e259c1951f5cfb
arnav130496 <arnav130496@gmail.com>
2024-11-18 11:16:01 +0530

    repo init

---
+++ src/main/java/com/learning/loans/entity/Loans.java
@@ -0,0 +1,23 @@
+package com.learning.loans.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loans {
+
+    private Long loanId;
+    private String mobileNumber;
+    private String loanNumber;
+    private String loanType;
+    private int totalLoan;
+    private int amountPaid;
+    private int outstandingAmount;
+}
